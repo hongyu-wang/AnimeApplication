fac20343037d2bcc6c83e23370d5e0ba9e9e4669
hongyu-wang <hongyu.wang@mail.utoronto.ca>
2017-07-09 17:27:15 -0400

    Added Google Gson Library

--- app/src/main/java/com/main/Main.java
+++ app/src/main/java/com/main/Main.java
@@ -13,15 +13,9 @@ import org.json.JSONObject;
  *
  * Created by hongy on 7/5/2017.
  */
-
 public class Main {
 
     public static void main (String args []){
-        ApiData data = new ApiImplementation();
-        //AMHmaSPrkHpAq6H8VxAUGZB5kZyc4TDoJaEbmQir
-        System.out.println("https://anilist.co/api/auth/access_token?grant_type=client_credentials&"
-                +"client_id" + data.getClientID() + "client_secret" + data.getClientSecret());
-
 
     }
 
