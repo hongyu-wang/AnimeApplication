6aefc89c28230d5f4df50606564f847cb484ada6
darknal43 <hongywang@hotmail.com>
2017-07-19 16:23:01 -0400

    Example with BasicSeriesModel

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -19,6 +19,7 @@ import com.android.volley.toolbox.Volley;
 import com.webservices.endpointBuilder.QueryString;
 import com.webservices.model.ClientCredModel;
 import com.webservices.model.ModelFactory;
+import com.webservices.model.seriesEndpoints.BasicSeriesModel;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -39,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "Main Activity Starting!");
         EventBus.getDefault().register(this);
+
         setContentView(R.layout.activity_main);
         // Set up the navigation drawer
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -67,6 +69,14 @@ public class MainActivity extends AppCompatActivity {
     public void onEvent(ClientCredModel model){
         Log.d(TAG, "ClientCredEvent");
         ModelFactory.setCurrentClient(model);
+        ModelFactory.requestModelList(this, BasicSeriesModel.class, "Code+Geass");
+
+    }
+
+    @Subscribe
+    public void onEvent(BasicSeriesModel [] model){
+
+        Log.d(TAG, "huh");
     }
 
     private void setupDrawerContent(NavigationView navView) {
